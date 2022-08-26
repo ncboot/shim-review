8dd641c7a015bc751bb58793fd52bbeabfd72211
ncboot <nkovach@ncomputing.com>
2022-08-26 12:24:08 +0300

    disable docker cache

--- make-shim.sh
+++ make-shim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -t shim-build .
+docker build --no-cache -t shim-build .
 docker run shim-build
 ID=$(docker ps -a | grep shim-build | awk '{print $1;}')
 docker cp $ID:/opt/shim/shimx64.efi ./
