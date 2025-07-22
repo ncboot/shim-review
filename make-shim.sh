1e8d0a8d708bb5a497b601ef46314f2ad6b10101
ncboot <nkovach@ncomputing.com>
2025-07-22 10:51:32 +0300

    modify make-shim.sh script to use final dir /opt/shim-16.0

--- make-shim.sh
+++ make-shim.sh
@@ -3,5 +3,5 @@
 docker build --no-cache -t shim-build .
 docker run shim-build
 ID=$(docker ps -a | grep shim-build | awk '{print $1;}')
-docker cp $ID:/opt/shim/shimx64.efi ./
+docker cp $ID:/opt/shim-16.0/shimx64.efi ./
 
