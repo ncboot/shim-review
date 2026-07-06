80240dff57ad73af5fc042d7f8725d8aef2bc411
ncboot <nkovach@ncomputing.com>
2026-07-06 13:51:19 +0300

    Improve make-shim.sh script for generating log file

--- make-shim.sh
+++ make-shim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build --no-cache -t shim-build .
-docker run shim-build
+docker buildx build --progress=plain --no-cache -t shim-build . 2>&1 | tee build.log
+docker run shim-build 2>&1 | tee -a build.log
 ID=$(docker ps -a | grep shim-build | awk '{print $1;}')
-docker cp $ID:/opt/shim-16.0/shimx64.efi ./
+docker cp $ID:/opt/shim-16.1/shimx64.efi ./
 
